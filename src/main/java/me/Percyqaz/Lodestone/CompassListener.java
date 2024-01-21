74f7baa38a71b2bc94b44ee454ce0acbd9cefd2b
percyqaz <21290233+percyqaz@users.noreply.github.com>
2024-01-21 01:32:54 +0000

    Fix maven build warning

--- src/main/java/me/Percyqaz/Lodestone/CompassListener.java
+++ src/main/java/me/Percyqaz/Lodestone/CompassListener.java
@@ -9,7 +9,6 @@ import org.bukkit.event.EventHandler;
 import org.bukkit.event.EventPriority;
 import org.bukkit.event.Listener;
 import org.bukkit.event.block.Action;
-import org.bukkit.event.entity.EntityInteractEvent;
 import org.bukkit.event.inventory.InventoryAction;
 import org.bukkit.event.inventory.InventoryClickEvent;
 import org.bukkit.event.player.PlayerInteractEntityEvent;
